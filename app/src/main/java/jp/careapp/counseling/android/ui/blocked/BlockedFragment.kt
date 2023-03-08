d5cacc6f8b200486477044369ee8c014ef092a42
danglvh2 <danglvh2@rikkeisoft.com>
2023-03-08 11:47:14 +0700

    Logic and layout BlockedFragment

--- app/src/main/java/jp/careapp/counseling/android/ui/blocked/BlockedFragment.kt
+++ app/src/main/java/jp/careapp/counseling/android/ui/blocked/BlockedFragment.kt
@@ -2,15 +2,10 @@ package jp.careapp.counseling.android.ui.blocked
 
 import android.view.View
 import androidx.fragment.app.viewModels
-import androidx.lifecycle.Observer
-import androidx.navigation.fragment.findNavController
 import jp.careapp.core.base.BaseFragment
 import jp.careapp.core.utils.dialog.CommonAlertDialog
-import jp.careapp.core.utils.executeAfter
 import jp.careapp.counseling.R
-import jp.careapp.counseling.android.data.network.FavoriteResponse
 import jp.careapp.counseling.databinding.FragmentBlockedBinding
-import jp.careapp.counseling.android.utils.event.EventObserver
 import dagger.hilt.android.AndroidEntryPoint
 import jp.careapp.counseling.android.navigation.AppNavigation
 import jp.careapp.counseling.android.utils.customView.ToolBarCommon
@@ -22,83 +17,65 @@ class BlockedFragment : BaseFragment<FragmentBlockedBinding, BlockedViewModel>()
     @Inject
     lateinit var appNavigation: AppNavigation
 
-    private val viewModels: BlockedViewModel by viewModels()
-    private lateinit var adapter: BlockedAdapter
     override val layoutId: Int = R.layout.fragment_blocked
-    override fun getVM(): BlockedViewModel = viewModels
-    private var blockeds: MutableList<FavoriteResponse> = mutableListOf()
+
+    private val mViewModel: BlockedViewModel by viewModels()
+    override fun getVM() = mViewModel
+
+    private var mAdapter: BlockedAdapter? = null
 
     override fun initView() {
         super.initView()
 
         setUpToolBar()
+
+        mAdapter = BlockedAdapter { mViewModel.onClickShowDialogConfirmDeleteBlock(it) }
+        binding.rvBlocked.adapter = mAdapter
     }
 
     override fun bindingStateView() {
         super.bindingStateView()
-        viewModels.forceRefresh()
-        adapter = BlockedAdapter(lifecycleOwner = viewLifecycleOwner, events = viewModels)
-        binding.apply {
-            rvBlocked.setHasFixedSize(true)
-            rvBlocked.adapter = adapter
+
+        mViewModel.isShowNoData.observe(viewLifecycleOwner) {
+            showNoData(it)
         }
-        viewModels.blockedLoading.observe(
-            viewLifecycleOwner,
-            Observer {
-                binding.executeAfter {
-                    loading = it
-                }
+
+        mViewModel.blockedList.observe(viewLifecycleOwner) {
+            mAdapter?.submitList(it)
+        }
+
+        mViewModel.mActionState.observe(viewLifecycleOwner) {
+            when (it) {
+                is BlockedActionState.ShowDialogConfirmDeleteBlock -> showDialogConfirmDeleteBlock(
+                    it.memberName,
+                    it.memberCode
+                )
             }
-        )
-        viewModels.error.observe(
-            viewLifecycleOwner,
-            Observer {
-            }
-        )
-        viewModels.uiBlocked.observe(
-            viewLifecycleOwner,
-            Observer {
-                it ?: return@Observer
-                blockeds.clear()
-                blockeds.addAll(it)
-                adapter.submitList(it.toMutableList())
-            }
-        )
-        viewModels.showDialogAction.observe(
-            viewLifecycleOwner,
-            EventObserver { data ->
-                CommonAlertDialog.getInstanceCommonAlertdialog(requireContext())
-                    .showDialog()
-                    .setDialogTitleWithString("${data.name} ${resources.getString(R.string.unblock)}")
-                    .setTextNegativeButton(R.string.cancel_block_alert)
-                    .setTextPositiveButton(R.string.to_release)
-                    .setOnNegativePressed {
-                        it.dismiss()
-                    }
-                    .setOnPositivePressed {
-                        viewModels.setCodeBlocked(data.code)
-                        if (blockeds.removeIf {
-                                it.code == data.code
-                            }
-                        )
-                            adapter.submitList(blockeds)
-                        if (blockeds.isEmpty())
-                            viewModels.isShowNoData.postValue(true)
-                        it.dismiss()
-                    }
-            }
-        )
-        viewModels.deleteBlockedLoading.observe(
-            viewLifecycleOwner,
-            Observer {
-                showHideLoading(it)
-            }
-        )
-        viewModels.isShowNoData.observe(viewLifecycleOwner, isShowNoDataObserver)
+        }
     }
 
-    private var isShowNoDataObserver: Observer<Boolean> = Observer {
-        showNoData(it)
+    private fun setUpToolBar() {
+        binding.toolBar.setOnToolBarClickListener(object : ToolBarCommon.OnToolBarClickListener() {
+            override fun onClickLeft() {
+                super.onClickLeft()
+                if (!isDoubleClick) appNavigation.navigateUp()
+            }
+        })
+    }
+
+    private fun showDialogConfirmDeleteBlock(memberName: String, memberCode: String) {
+        CommonAlertDialog.getInstanceCommonAlertdialog(requireContext())
+            .showDialog()
+            .setDialogTitleWithString("$memberName ${resources.getString(R.string.unblock)}")
+            .setTextNegativeButton(R.string.cancel_block_alert)
+            .setTextPositiveButton(R.string.to_release)
+            .setOnNegativePressed {
+                it.dismiss()
+            }
+            .setOnPositivePressed {
+                it.dismiss()
+                mViewModel.deleteBlocked(memberCode)
+            }
     }
 
     private fun showNoData(isShow: Boolean) {
@@ -111,12 +88,9 @@ class BlockedFragment : BaseFragment<FragmentBlockedBinding, BlockedViewModel>()
         }
     }
 
-    private fun setUpToolBar() {
-        binding.toolBar.setOnToolBarClickListener(object : ToolBarCommon.OnToolBarClickListener() {
-            override fun onClickLeft() {
-                super.onClickLeft()
-                if (!isDoubleClick) appNavigation.navigateUp()
-            }
-        })
+    override fun onDestroyView() {
+        binding.rvBlocked.adapter = null
+        mAdapter = null
+        super.onDestroyView()
     }
 }
