f9a72b8656de1c1d12c400147c3941f39c9c720e
danglvh2 <danglvh2@rikkeisoft.com>
2023-03-26 15:39:19 +0700

    Fix layout RMStartFragment

--- app/src/main/java/jp/careapp/counseling/android/ui/review_mode/enterName/RMEnterNameFragment.kt
+++ app/src/main/java/jp/careapp/counseling/android/ui/review_mode/enterName/RMEnterNameFragment.kt
@@ -27,7 +27,6 @@ class RMEnterNameFragment : BaseFragment<FragmentRmEnterNameBinding, RMEnterName
     override fun initView() {
         super.initView()
 
-        binding.edtNickName.setHint(getString(R.string.nick_name))
         binding.edtNickName.setTextChangeListener { count ->
             binding.btnSubmit.isEnabled = count != 0
         }
@@ -57,8 +56,8 @@ class RMEnterNameFragment : BaseFragment<FragmentRmEnterNameBinding, RMEnterName
                 CommonAlertDialog.getInstanceCommonAlertdialog(requireContext())
                     .showDialog()
                     .setDialogTitle(R.string.your_name_not_enter)
-                    .setTextPositiveButton(R.string.ok)
-                    .setOnPositivePressed {
+                    .setTextOkButton(R.string.ok)
+                    .setOnOkButtonPressed {
                         it.dismiss()
                     }
             }
