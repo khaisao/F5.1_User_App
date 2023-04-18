76b223d21cf3bb7df93e13e4dda4f82b2ceb073a
danglvh2 <danglvh2@rikkeisoft.com>
2023-04-18 16:57:21 +0700

    Change layout maintenance

--- app/src/main/java/jp/careapp/counseling/android/ui/maintenance/MaintenanceViewModel.kt
+++ app/src/main/java/jp/careapp/counseling/android/ui/maintenance/MaintenanceViewModel.kt
@@ -1,6 +1,8 @@
 package jp.careapp.counseling.android.ui.maintenance
 
+import dagger.hilt.android.lifecycle.HiltViewModel
 import jp.careapp.core.base.BaseViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class MaintenanceViewModel @Inject constructor() : BaseViewModel()
