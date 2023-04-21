b69f828374a3d45bdabef8b1abe91268b4dd9f4f
danglvh2 <danglvh2@rikkeisoft.com>
2023-04-21 15:07:47 +0700

    Fix bug show loading hyperlink webView

--- app/src/main/java/jp/careapp/counseling/android/ui/webView/WebViewFragment.kt
+++ app/src/main/java/jp/careapp/counseling/android/ui/webView/WebViewFragment.kt
@@ -6,18 +6,15 @@ import android.graphics.Bitmap
 import android.net.Uri
 import android.webkit.*
 import androidx.activity.OnBackPressedCallback
-import androidx.annotation.StringRes
 import androidx.fragment.app.viewModels
 import dagger.hilt.android.AndroidEntryPoint
 import jp.careapp.core.base.BaseFragment
-import jp.careapp.core.utils.dialog.CommonAlertDialog
 import jp.careapp.counseling.BuildConfig
 import jp.careapp.counseling.R
 import jp.careapp.counseling.android.data.model.user_profile.ActionLoadProfile
 import jp.careapp.counseling.android.data.pref.AppPreferences
 import jp.careapp.counseling.android.navigation.AppNavigation
 import jp.careapp.counseling.android.utils.Define
-import jp.careapp.counseling.android.utils.Define.Companion.URL_FREE_POINT
 import jp.careapp.counseling.android.utils.customView.ToolBarCommon
 import jp.careapp.counseling.databinding.FragmentWebViewBinding
 import org.greenrobot.eventbus.EventBus
@@ -36,12 +33,11 @@ class WebViewFragment : BaseFragment<FragmentWebViewBinding, WebViewViewModel>()
     private val viewModel: WebViewViewModel by viewModels()
     override fun getVM() = viewModel
 
-    private lateinit var webViewTitle:  Map<String, String>
+    private lateinit var webViewTitle: Map<String, String>
 
     override fun initView() {
         super.initView()
-        if (BuildConfig.DEBUG)
-            WebView.setWebContentsDebuggingEnabled(true)
+        if (BuildConfig.DEBUG) WebView.setWebContentsDebuggingEnabled(true)
 
         webViewTitle = mapOf(
             "/payment/methods" to requireContext().getString(R.string.methods_title),
@@ -71,7 +67,41 @@ class WebViewFragment : BaseFragment<FragmentWebViewBinding, WebViewViewModel>()
                 val urlWebView = it.getString(Define.URL_WEB_VIEW).toString()
                 binding.webView.apply {
                     settings.domStorageEnabled = true
-                    webViewClient = getWebViewClient(urlWebView)
+                    webViewClient = object : WebViewClient() {
+                        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
+                            super.onPageStarted(view, url, favicon)
+                            startLoading()
+                        }
+
+                        override fun onPageFinished(view: WebView?, url: String?) {
+                            super.onPageFinished(view, url)
+                            endLoading()
+                        }
+
+                        override fun shouldOverrideUrlLoading(
+                            view: WebView?,
+                            request: WebResourceRequest?
+                        ): Boolean {
+                            val url = request?.url.toString()
+                            try {
+                                when {
+                                    URLUtil.isNetworkUrl(url) -> {
+                                        view?.loadUrl(url)
+                                    }
+                                    Define.CALL_BACK_BUY_POINT_GOOGLE_ == url -> {
+                                        appNavigation.openWebBuyPointToBuyPointGoogle()
+                                    }
+                                    else -> {
+                                        val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
+                                        startActivity(intent)
+                                    }
+                                }
+                            } catch (e: Exception) {
+                                Timber.e(e)
+                            }
+                            return true
+                        }
+                    }
                     settings.javaScriptEnabled = true
                     clearCache(true)
                     loadUrl(urlWebView)
@@ -89,21 +119,6 @@ class WebViewFragment : BaseFragment<FragmentWebViewBinding, WebViewViewModel>()
         }
 
         handleBackPress()
-    }
-
-    private fun loadJs(webView: WebView, script: String) {
-        webView.loadUrl(
-            "javascript:" +
-                    "function addScriptTag() { " +
-                    "\n  var headTag = document.getElementsByTagName(\"head\")[0]; " +
-                    "\n  var scriptTag = document.createElement(\"script\"); " +
-                    "\n  var content = document.createTextNode('$script');  " +
-                    "\n  scriptTag.appendChild(content); " +
-                    "\n  headTag.appendChild(scriptTag); " +
-                    " \n}" +
-                    "\n addScriptTag();" +
-                    ""
-        )
     }
 
     private fun setTitleByUrl(urlWebView: String) {
@@ -129,124 +144,36 @@ class WebViewFragment : BaseFragment<FragmentWebViewBinding, WebViewViewModel>()
         showHideLoading(false)
     }
 
-    private fun getWebViewClient(urlWebView: String): WebViewClient {
-        val token = try {
-            preferences.getToken()?.substring(7)
-        } catch (e: Exception) {
-            e.printStackTrace()
-            ""
-        }
-        val env = if (BuildConfig.BUILD_TYPE == "release") "PRODUCTION" else "STAGING"
-        val scriptFreePoint =
-            "var token=\"${token}\"; var domain = \"${BuildConfig.BASE_URL}\"; var env = \"${env}\"; handleFreePointView();"
-        val scriptBuyPoint =
-            "var token=\"${token}\"; var domain = \"${BuildConfig.BASE_URL}\"; var env = \"${env}\"; handlePointInfo(); var store = \"googleplay\""
-        return object : WebViewClient() {
-            override fun onPageStarted(
-                view: WebView?,
-                url: String?,
-                favicon: Bitmap?
-            ) {
-                super.onPageStarted(view, url, favicon)
-                startLoading()
-            }
-
-            override fun onPageFinished(view: WebView, url: String?) {
-                super.onPageFinished(view, url)
-                endLoading()
-                if (urlWebView == URL_FREE_POINT)
-                    loadJs(view, scriptFreePoint)
-                else
-                    loadJs(view, scriptBuyPoint)
-            }
-
-            override fun shouldOverrideUrlLoading(
-                view: WebView,
-                request: WebResourceRequest
-            ): Boolean {
-                val url = request.url.toString()
-                try {
-                    when {
-                        URLUtil.isNetworkUrl(url) -> {
-                            view.loadUrl(url)
-                        }
-                        Define.CALL_BACK_BUY_POINT_GOOGLE_ == url -> {
-                            appNavigation.openWebBuyPointToBuyPointGoogle()
-                        }
-                        else -> {
-                            val intent = Intent(Intent.ACTION_VIEW, Uri.parse(url))
-                            startActivity(intent)
-                        }
-                    }
-                } catch (e: Exception) {
-                    Timber.e(e)
-                }
-                return true
-            }
-        }
-    }
-
-    private fun showBuyPointSuccessDialog(@StringRes title: Int) {
-        CommonAlertDialog.getInstanceCommonAlertdialog(requireContext())
-            .showDialog()
-            .setDialogTitle(title)
-            .setTextPositiveButton(jp.careapp.core.R.string.text_OK)
-            .setOnPositivePressed {
-                it.dismiss()
-            }
-    }
-
-    private inner class Client : WebViewClient() {
-        override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
-            super.onPageStarted(view, url, favicon)
-            startLoading()
-        }
-
-        override fun onPageFinished(view: WebView?, url: String?) {
-            super.onPageFinished(view, url)
-            endLoading()
-        }
-
-        override
-        fun shouldOverrideUrlLoading(view: WebView, url: String): Boolean {
-            view.loadUrl(url)
-            return true
-        }
-    }
-
     override fun setOnClick() {
         super.setOnClick()
-        binding.toolBar.setOnToolBarClickListener(
-            object : ToolBarCommon.OnToolBarClickListener() {
-                override fun onClickLeft() {
-                    super.onClickLeft()
-                    if (binding.webView.canGoBack()) {
-                        startLoading()
-                        binding.webView.goBack()
-                    } else {
-                        appNavigation.navigateUp()
-                    }
-                }
 
-                override fun onClickRight() {
-                    super.onClickRight()
-                    appNavigation.navController?.let {
-                        notifyUpdateIfNeed()
-                        it.popBackStack()
-                    }
-                }
-            }
-        )
-    }
-
-    private fun handleBackPress() {
-        val callback: OnBackPressedCallback =
-            object : OnBackPressedCallback(true) {
-                override fun handleOnBackPressed() {
-                    notifyUpdateIfNeed()
+        binding.toolBar.setOnToolBarClickListener(object : ToolBarCommon.OnToolBarClickListener() {
+            override fun onClickLeft() {
+                super.onClickLeft()
+                if (binding.webView.canGoBack()) {
+                    binding.webView.goBack()
+                } else {
                     appNavigation.navigateUp()
                 }
             }
+
+            override fun onClickRight() {
+                super.onClickRight()
+                appNavigation.navController?.let {
+                    notifyUpdateIfNeed()
+                    it.popBackStack()
+                }
+            }
+        })
+    }
+
+    private fun handleBackPress() {
+        val callback: OnBackPressedCallback = object : OnBackPressedCallback(true) {
+            override fun handleOnBackPressed() {
+                notifyUpdateIfNeed()
+                appNavigation.navigateUp()
+            }
+        }
         requireActivity().onBackPressedDispatcher.addCallback(this, callback)
     }
 
